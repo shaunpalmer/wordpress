a3ee6c8c3a6a1519dc1a70b775db7f56764fff4a
shaun palmer <shaun.palmer0@gmail.com>
2020-10-01 01:46:31 +1300

    Add files via upload

---
+++ sp_sp_superclean/sp_sp_superclean/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * SuperClean Pro Theme Customizer
+ *
+ * @package SuperClean_Pro
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function sp_sp_superclean_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'sp_sp_superclean_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'sp_sp_superclean_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'sp_sp_superclean_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function sp_sp_superclean_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function sp_sp_superclean_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function sp_sp_superclean_customize_preview_js() {
+	wp_enqueue_script( 'sp_sp_superclean-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'sp_sp_superclean_customize_preview_js' );
